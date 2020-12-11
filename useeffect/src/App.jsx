cd36ce1adedea6b6eb647a34656cda1881ea0ae6
Lance Sanchez <lancesanchez@outlook.com>
2020-12-11 13:28:57 -0500

    BAD BAD BAD

--- useeffect/src/App.jsx
+++ useeffect/src/App.jsx
@@ -1,13 +1,34 @@
 import './App.css';
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import CharacterList from "./components/CharacterList"
+import SearchBar from "./components/SearchBar"
 import axios from 'axios'
+
+const filterCharacters = (searchTerm) => {
+  const char = characters.filter((el) => {
+    el.name.toLowerCase().includes(searchTerm.toLowerCase())
+  }
+  )
+  return char
+}
 
 
 function App() {
 
+  // const handleChange = async(e) => {
+  //   if()
+  // }
+
+  const handleChange = async(e) =>{
+    if(!e.target.value){
+      setSearchTerm("")
+      }
+      const char = await searchTerm(e.target.value)
+        searchTerm(e.target.value,char)
+      } 
 
   const [characters, setCharacters] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
   axios.get('https://rickandmortyapi.com/api/character')
@@ -18,7 +39,8 @@ function App() {
 
   return (
     <div>
-    <CharacterList characters = {characters}/>
+      <SearchBar characters ={characters}/>
+      <CharacterList characters = {characters}/>
     </div>
   );
 }
---
+++ useeffect/src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from "react "
+
+export default function SearchBar(props){
+    return (
+        <div>
+            <input type = "text"/>
+            <button>Search</button> 
+        </div>
+    )
+}
